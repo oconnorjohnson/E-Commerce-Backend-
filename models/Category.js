9253d661b700286da738799926092530ca9a8bfc
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-06 14:10:47 -0700

    worked on models/category.js

--- models/Category.js
+++ models/Category.js
@@ -4,17 +4,24 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false, 
+    primaryKey: true, 
+    autoIncrememnt: true
+    },
+  category_name: {
+    type: DataTypes.STRING, 
+    allowNull: false
   }
-);
+  },  {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'category',
+    }
+  );
 
 module.exports = Category;
