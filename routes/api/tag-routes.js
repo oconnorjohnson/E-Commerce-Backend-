c6555c621ed3c0496792e045153e6fdd1393c069
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-06 19:59:37 -0700

    finished tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,9 +21,8 @@ router.get('/', (req, res) => {
   }
 });
 
+// Find a single tag by its 'id'
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: 
@@ -42,16 +41,51 @@ router.get('/:id', (req, res) => {
   }
 });
 
+// Create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
+// Update tag's name by its 'id' value 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      });
+      if(!updatedTag[0]) {
+        res.status(404).json({message: "No tags found with that ID..."});
+      }
+      res.status(200).json(updatedTag);
+    } catch (error) {
+      res.status(500).json(error)
+    }
 });
 
+// Delete tag by its 'id' value 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    if(!deletedTag) {
+      res.status(404).json({message: "No tags found with that ID..."})
+    }
+    res.status(200).json(deletedTag);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
