16180a4556390deaf2dd39a388768479097c9b0a
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-06 19:25:49 -0700

    add get route to find a single tag by its 'id'

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,9 +5,12 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{
+      include: 
+      [
+        {
         model: Product
-      }]
+        }
+      ]
     });
     if(!tagData) {
       res.status(404).json({message: "No tags found in database..."});
@@ -21,6 +24,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: 
+      [
+        {
+          model: Product 
+        }
+      ]
+    });
+    if(!tagData) {
+      res.status(404).json({message: "No tags found with that ID..."})
+    }
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 router.post('/', (req, res) => {
